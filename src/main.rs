c7cdec19bb0ff0f45e324543d08f14773e2ce733
ihor <avramenko.ihor@chnu.edu.ua>
2025-10-20 17:25:41 +0300

    remove goodbye message

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,7 @@
 /// this is the best implementation of this program to ever exist.
 
 fn main() {
-    print("Hello, world!");
-    print("Goodbye, world!");
-}
+    print_hello();
 
 // a function that prints a message
 fn print(m: &str) {
